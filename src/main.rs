1e61985f101b78ed1f9f0bc9ba1b2bc458f30e5f
Austin Gill <Notgnoshi@gmail.com>
2024-04-18 18:39:45 -0500

    Add bare minimal logging implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod submod;
 
+use tracing_subscriber::EnvFilter;
+
 fn main() {
+    let filter = EnvFilter::builder()
+        // placeholder for CLI argument
+        .with_default_directive(tracing::Level::DEBUG.into())
+        .from_env_lossy();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_ansi(true)
+        .init();
+
     tracing::trace!("trace");
     tracing::debug!("debug");
     tracing::info!("info");
